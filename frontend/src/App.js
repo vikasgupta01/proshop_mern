4dcc475dd791e53e065512aeae55b156aa29d689
vikasgupta01 <vikasgupta2552@gmail.com>
2021-07-25 19:20:23 +0530

    Implement React Router and add Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,23 @@ import { Container } from "react-bootstrap";
 import Footer from "./components/Footer"
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <main className='py-3'>
-        <Container>
-          <HomeScreen />
-        </Container>
-      </main>
-      <Footer />
-    </>
+    <Router>
+      <>
+        <Header />
+        <main className='py-3'>
+          <Container>
+            <Route path='/' component={HomeScreen} exact />
+            <Route path='/product/:id' component={ProductScreen} />
+          </Container>
+        </main>
+        <Footer />
+      </>
+    </Router>
   );
 }
 
