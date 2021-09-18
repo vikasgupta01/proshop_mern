930124e7b68ee63ab2cae4f3ac1e91e7398b1c9d
vikasgupta01 <vikasgupta2552@gmail.com>
2021-09-18 19:49:31 +0530

    Redux Implementation

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
-      useCreateIndex: true,
+      // useCreateIndex: true,
     });
 
     console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
@@ -13,6 +13,6 @@ const connectDB = async () => {
     console.log(`Error: ${error.message}`.red.underline.bold);
     process.exit(1);
   }
-}
+};
 
 export default connectDB;
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,76 +1,100 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
-import Rating from '../components/Rating'
-import axios from 'axios'
-// import products from '../products'
+import React, { useEffect } from "react";
+import { Button, Card, Col, Image, ListGroup, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getProductDetails } from "../actions/productActions";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import Rating from "../components/Rating";
 
 const ProductScreen = (props) => {
   //// we're gonna use useState instead, and fetch product from backend rather than getting it from
   //// frontend only where we havt to store products.js file.
   // const product = products.find((p) => p._id === props.match.params.id)
 
-  const [product, setProduct] = useState({})
+  const dispatch = useDispatch();
+
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${props.match.params.id}`)
-
-      setProduct(data);
-    }
-    fetchProduct();
-  }, [props.match.params.id])
+    dispatch(getProductDetails(props.match.params.id));
+  }, [dispatch, props.match.params.id]);
 
   return (
     <div>
-      <Link className='btn btn-light my-3' to='/'>Go Back</Link>
-      <Row>
-        <Col md={6}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-        <Col md={3}>
-          <ListGroup variant='flush'>
-            <ListGroup.Item>
-              <h4>{product.name}</h4>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating value={product.rating} count={product.numReviews} color={'yellow'} />
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <h5>Price: ${product.price}</h5>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <p><strong>Product Description: </strong>{product.description}</p>
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
-            <ListGroup variant='flush'>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={3}>
+            <ListGroup variant="flush">
               <ListGroup.Item>
-                <Row>
-                  <Col>Price:</Col>
-                  <Col><strong>${product.price}</strong></Col>
-                </Row>
+                <h4>{product.name}</h4>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</Col>
-                </Row>
+                <Rating
+                  value={product.rating}
+                  count={product.numReviews}
+                  color={"yellow"}
+                />
               </ListGroup.Item>
               <ListGroup.Item>
-                <Row>
-                  <Button className='btn-block' type='button' disabled={!product.countInStock > 0}>Add To Cart</Button>
-                </Row>
+                <h5>Price: ${product.price}</h5>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <p>
+                  <strong>Product Description: </strong>
+                  {product.description}
+                </p>
               </ListGroup.Item>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
-      {/* <Card></Card> */}
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Button
+                      className="btn-block"
+                      type="button"
+                      disabled={!product.countInStock > 0}
+                    >
+                      Add To Cart
+                    </Button>
+                  </Row>
+                </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ProductScreen
+export default ProductScreen;
