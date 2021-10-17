01b228b9b83ff4540210814e18b43b94855a97ed
vikasgupta01 <vikasgupta2552@gmail.com>
2021-10-17 14:47:27 +0530

    Controllers, and generate JWT for backend auth

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,34 +1,19 @@
 import express from "express";
-import expressAsyncHandler from "express-async-handler";
-import Product from "../models/productModel.js";
+import {
+  getProductById,
+  getProducts,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
 // @desc    Fetch all products
 // @route   GET /api/products
 // @access  Public
-router.get(
-  "/",
-  expressAsyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+router.route("/").get(getProducts);
 
 // @desc    Fetch single product
 // @route   GET /api/products
 // @access  Public
-router.get(
-  "/:id",
-  expressAsyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product Not Found");
-    }
-  })
-);
+router.route("/:id").get(getProductById);
 
 export default router;
