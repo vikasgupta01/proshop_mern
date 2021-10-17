c2f60fcc7b4d025ef8ee2f23cef1fe93fc69d041
vikasgupta01 <vikasgupta2552@gmail.com>
2021-10-17 18:27:29 +0530

    Custom Auth middleware, user registration & password encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,4 +24,56 @@ const authUser = expressAsyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// @desc Register new user
+// route POST /api/users
+// @access Public
+const registerUser = expressAsyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status = 400;
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User data");
+  }
+});
+
+// @desc Get user profile
+// @route GET /api/users/profile
+// @access Private
+const getUserProfile = expressAsyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found!");
+  }
+});
+
+export { authUser, registerUser, getUserProfile };
