bfc2afce1e1f996ce5e4ca5812ad87e45c8d15ff
vikasgupta01 <vikasgupta2552@gmail.com>
2022-02-06 09:20:50 +0530

    Update order to paid Endpoint

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -24,9 +24,10 @@ const OrderScreen = ({ match }) => {
   }
 
   useEffect(() => {
-    dispatch(getOrderDetails(orderId));
-    // eslint-disable-next-line
-  }, []);
+    if (!order || order._id !== orderId) {
+      dispatch(getOrderDetails(orderId));
+    }
+  }, [dispatch, order, orderId]);
 
   return loading ? (
     <Loader />
