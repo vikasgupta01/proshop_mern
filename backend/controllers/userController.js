44a49ec377216f0d65d3a50c54766c84a3c714c1
vikasgupta01 <vikasgupta2552@gmail.com>
2022-02-08 02:25:44 +0530

    checkAdmin middleware & get users endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -104,4 +104,12 @@ const updateUserProfile = expressAsyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, registerUser, getUserProfile, updateUserProfile };
+// @desc Get all users
+// @route GET /api/users
+// @access Private/Admin
+const getUsers = expressAsyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,13 +2,14 @@ import express from "express";
 import {
   authUser,
   getUserProfile,
+  getUsers,
   registerUser,
   updateUserProfile,
 } from "../controllers/userController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { checkAdmin, protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect, checkAdmin, getUsers);
 router.post("/login", authUser);
 router.route("/profile").get(protect, getUserProfile);
 router
