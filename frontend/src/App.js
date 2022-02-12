b179e5afc6f0ee9f98d6f96c26119afec18d2343
vikasgupta01 <vikasgupta2552@gmail.com>
2022-02-12 23:44:53 +0530

    Product List - Admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
+import ProductListScreen from "./screens/ProductListScreen";
 
 const App = () => {
   return (
@@ -35,6 +36,11 @@ const App = () => {
             <Route
               path="/admin/user/:id/edit"
               component={UserEditScreen}
+              exact
+            />
+            <Route
+              path="/admin/productlist"
+              component={ProductListScreen}
               exact
             />
             <Route path="/" component={HomeScreen} exact />
