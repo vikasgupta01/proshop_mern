ebae57c279d401a428562c92cf65aea4cae69ecb
vikasgupta01 <vikasgupta2552@gmail.com>
2022-02-12 12:02:29 +0530

    User delete backend. Admin only.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -112,4 +112,25 @@ const getUsers = expressAsyncHandler(async (req, res) => {
   res.json(users);
 });
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers };
+// @desc Delete user
+// @route DELETE /api/users/:id
+// @access Private/Admin
+const deleteUser = expressAsyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+  if (user) {
+    user.remove();
+    res.json({ message: "User Removed" });
+  } else {
+    res.status(404);
+    throw new Error("User not found!");
+  }
+});
+
+export {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+};
