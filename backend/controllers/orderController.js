956d655f644c2c15b5146028d0f1de85f745419b
vikasgupta01 <vikasgupta2552@gmail.com>
2022-02-17 21:46:47 +0530

    Mark order as delivered - backend : Admin Only

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -91,10 +91,28 @@ const getOrders = expressAsyncHandler(async (req, res) => {
   res.json(orders);
 });
 
+// @desc    Update order to delivered
+// @route   PUT /api/orders/:id/deliver
+// @access  Private/Admin
+const updateOrderToDelivered = expressAsyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
 export {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  updateOrderToDelivered,
   getMyOrders,
   getOrders,
 };
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,9 +2,10 @@ import express from "express";
 import {
   addOrderItems,
   getOrderById,
-  updateOrderToPaid,
   getMyOrders,
   getOrders,
+  updateOrderToPaid,
+  updateOrderToDelivered,
 } from "../controllers/orderController.js";
 import { protect, checkAdmin } from "../middleware/authMiddleware.js";
 const router = express.Router();
@@ -16,5 +17,6 @@ router
 router.route("/myorders").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
+router.route("/:id/deliver").put(protect, checkAdmin, updateOrderToDelivered);
 
 export default router;
