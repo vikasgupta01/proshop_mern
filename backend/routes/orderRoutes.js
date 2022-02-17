edb25277bad81249db5d79c3b8b1b5e233afbe38
vikasgupta01 <vikasgupta2552@gmail.com>
2022-02-17 19:20:52 +0530

    Get all orders backend endpoint: Admin only

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,11 +4,15 @@ import {
   getOrderById,
   updateOrderToPaid,
   getMyOrders,
+  getOrders,
 } from "../controllers/orderController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect, checkAdmin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
-router.route("/").post(protect, addOrderItems);
+router
+  .route("/")
+  .post(protect, addOrderItems)
+  .get(protect, checkAdmin, getOrders);
 router.route("/myorders").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
